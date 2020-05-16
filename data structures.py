1844c74060198458ee1f08cf3e24f18c491c0cb6
Dharmikj2001 <64887695+Dharmikj2001@users.noreply.github.com>
2020-05-16 11:32:13 +0530

    Add files via upload

---
+++ data structures.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#assigning elements to different lists
+animals=["dog","cat","lion","tiger"]
+animals.append('snake')
+
+
+# In[3]:
+
+
+animals
+
+
+# In[5]:
+
+
+animals.insert(3,'elephant')
+
+
+# In[7]:
+
+
+animals
+
+
+# In[8]:
+
+
+#accessing elements from a tuple 
+randomtuple=('tokyo','nairobi','denver','rio')
+
+
+# In[9]:
+
+
+randomtuple[0]
+
+
+# In[10]:
+
+
+randomtuple[2]
+
+
+# In[11]:
+
+
+#deleting different dictionary elements 
+mydict={
+    "hello":1,
+    "bye":2,
+    "gmrng":3,
+    "gni8":4
+}
+
+
+# In[12]:
+
+
+mydict
+
+
+# In[15]:
+
+
+del mydict["hello"]
+
+
+# In[16]:
+
+
+mydict
+
+
+# In[ ]:
+
+
+
+
